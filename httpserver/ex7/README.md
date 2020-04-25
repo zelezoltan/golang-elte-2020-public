d6f61b800f6c168d91960776e853f820404dac8e
Gerifield <gergely.radics1@ibm.com>
2020-04-25 09:33:14 +0200

    feat: added sqlite support along mysql

--- httpserver/ex7/README.md
+++ httpserver/ex7/README.md
@@ -4,10 +4,14 @@ Here you see a bit more complex example with global context handling like db con
 
 I'll use some 3rd party libraries here and a db connection:
 - [Chi](https://github.com/go-chi/chi) - Easier HTTP routing
-- [MySQL driver](https://github.com/go-sql-driver/mysql) - MySQL driver for the built in `database/sql` interfaces
 - [SQLx](https://github.com/jmoiron/sqlx) - Small additional lib for easier SQL queries (result auto mapping to structs)
-- [Zap](https://github.com/uber-go/zap) - Better, structured logger 
+- [Zap](https://github.com/uber-go/zap) - Better, structured logger
 
+- [Go SQL Drivers](https://github.com/golang/go/wiki/SQLDrivers) - SQL drivers
+- [MySQL driver](https://github.com/go-sql-driver/mysql) - MySQL driver for the built in `database/sql` interfaces
+- [SQLite driver](https://github.com/mattn/go-sqlite3) - SQLite driver for the built in `database/sql` interfaces
+
+# Mysql setup
 DB setup I use here:
 ```
 Server: 127.0.0.1
@@ -28,10 +32,29 @@ CREATE TABLE `message` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 ```
 
+# SQLite setup
+
+Nothing to do here, I've added a small pre-build database file called `sqlite.db`.
+
+SQLite structure:
+
+```sqlite
+CREATE TABLE "message" (
+	"id"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	"name"	TEXT NOT NULL,
+	"message"	TEXT NOT NULL,
+	"created"	TEXT NOT NULL
+);
+```
+
+
 Run the server:
 ```
 $ go run httpserver/ex7/server.go httpserver/ex7/app.go
 ```
+
+The first run takes a while, because the sqlite driver uses CGO and it'll compile the SQLite C libraries.
+(You could use the `-v` flag to see what happens.)
 
 
 Do some HTTP calls:
--- httpserver/ex7/server.go
+++ httpserver/ex7/server.go
@@ -7,14 +7,24 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi"
-	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
 	"go.uber.org/zap"
+
+	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+const (
+	dbMysql  = "mysql"
+	dbSqlite = "sqlite"
 )
 
 func main() {
 	addr := flag.String("listen", ":8080", "Listening address")
-	dbConfig := flag.String("db", "root:root@tcp(127.0.0.1:3306)/testdb?parseTime=true", "Database connection string")
+
+	mysqlConfig := flag.String("mysqldb", "root:root@tcp(127.0.0.1:3306)/testdb?parseTime=true", "Mysql database connection string")
+	sqliteConfig := flag.String("sqlitedb", "file:httpserver/ex7/sqlite.db?_loc=auto&cache=shared", "SQLite database connection string")
+	db := flag.String("db", "sqlite", "Supported database (sqlite, mysql)")
 	flag.Parse()
 
 	logger, err := zap.NewProduction()
@@ -23,13 +33,30 @@ func main() {
 	}
 	defer logger.Sync() // flushes buffer, if any
 
-	db, err := sqlx.Connect("mysql", *dbConfig) // Connect also sends a Ping to check the server Open won't do that!
+	if *db != dbMysql && *db != dbSqlite {
+		log.Fatalln("Invalid db selection. Supported: mysql, sqlite")
+		return
+	}
+
+	// Setup the DB connection
+	var sqlxConn *sqlx.DB
+	if *db == dbMysql {
+		sqlxConn, err = sqlx.Connect("mysql", *mysqlConfig) // Connect also sends a Ping to check the server Open won't do that!
+	} else {
+		sqlxConn, err = sqlx.Connect("sqlite3", *sqliteConfig) // Connect also sends a Ping to check the server Open won't do that!
+	}
 	if err != nil {
 		logger.Fatal("database connection failure", zap.Error(err))
 	}
-	defer db.Close()
+	defer sqlxConn.Close()
 
-	app := NewApp(logger, db)
+	if *db == dbSqlite {
+		// Limit the concurrent connections when using sqlite
+		sqlxConn.SetMaxOpenConns(1)
+	}
+
+	// Setup the app part
+	app := NewApp(logger, sqlxConn)
 
 	r := chi.NewRouter()
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
@@ -38,6 +65,7 @@ func main() {
 
 	r.Mount("/api", app.Routes(chi.NewRouter()))
 
+	// Start the HTTP server
 	logger.Info("Listening", zap.String("addr", *addr))
 	err = http.ListenAndServe(*addr, r)
 	if err != nil {

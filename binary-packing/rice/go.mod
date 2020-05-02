09b8e4ed100d6560d8de8d6b4eba7ae5f79b8665
Gerifield <gergely.radics1@ibm.com>
2020-05-02 23:28:06 +0200

    feat: added example for binary packing

---
+++ binary-packing/rice/go.mod
@@ -0,0 +1,5 @@
+module github.com/gerifield/golang-elte-2020-public/binary-packing/rice
+
+go 1.14
+
+require github.com/GeertJohan/go.rice v1.0.0
---
+++ binary-packing/rice/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"net/http"
+
+	rice "github.com/GeertJohan/go.rice"
+)
+
+func main() {
+	addr := flag.String("addr", ":8080", "HTTP Listen addre")
+	flag.Parse()
+
+	box := rice.MustFindBox("static")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fp, err := box.Open("gopher.png")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer fp.Close()
+		_, _ = io.Copy(w, fp)
+	})
+
+	fmt.Println("Listening", *addr)
+	_ = http.ListenAndServe(*addr, nil)
+}
